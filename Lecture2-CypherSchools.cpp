869d7ce8b873c4c852c3803a56efb118e6776c38
Bhanvi Verma <113832473+bhanviverma24@users.noreply.github.com>
2023-10-07 16:58:01 +0530

    Update Lecture2-CypherSchools.cpp

--- Lecture2-CypherSchools.cpp
+++ Lecture2-CypherSchools.cpp
@@ -1,1 +1,7 @@
+#include<iostream> //This is a header
+using namespace std;
+int main(){ //we have written a main function
+cout<<"Hello world"; //this will print Hello world on the screen
+return 0;
+}
 
