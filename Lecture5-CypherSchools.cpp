da047d65caaae08a5fd79fca42aab92468ea4f3a
Bhanvi Verma <113832473+bhanviverma24@users.noreply.github.com>
2023-10-10 00:15:28 +0530

    Add files via upload

---
+++ Lecture5-CypherSchools.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+//    int x;  //variable can store single integer
+//    //arrays can be used to store multiple values in single variable name.
+//    int a[5]; // an array of name a  can store 6 integers.as array stars from 0 not 1
+//    //elements from 0-5
+//    //1st el will have index 0-->a[0]
+
+int main()
+{
+    int a,b,c,d,e;
+    cout<<"give 5 integers a s input"<<endl;
+    cin>>a>>b>>c>>d>>e;
+    cout<<"The integers in reverse order are: "<<e<<d<<c<<b<<a;
+return 0;
+}
