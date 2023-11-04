8858b36dbe5a5ec90f7d3e242ff2e2dcb152e087
Bhanvi Verma <113832473+bhanviverma24@users.noreply.github.com>
2023-11-04 11:44:38 +0530

    Add files via upload

---
+++ Lecture15(2)-CypherSchools.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+//int main(){
+//    string name;
+//    cin>>name;
+//    for(int i=0;i<5;i++){
+//        cout<<name[i]<<endl;
+//    }
+//}
+
+int main()
+{
+    string a;
+    getline(cin,a);
+    cout<<"Your full name is: "<<a<<endl;
+//    string a,b,c;
+//int a,b,c;
+//    cin>>a>>b;
+//    cout<<"The string which ypu have guven as input are "<<a<<b;
+//
+//
+//    c=a+b;
+//    cout<<"The combined word is : "<<c;
+//    cout<<"The length of the combined word is : "<<c.length();
+//    return 0;
+}
