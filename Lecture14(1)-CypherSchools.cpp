780e27c86c67473d7e99f23601ccd546568e23fa
Bhanvi Verma <113832473+bhanviverma24@users.noreply.github.com>
2023-11-04 11:01:38 +0530

    Add files via upload

---
+++ Lecture14(1)-CypherSchools.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+//1.
+//void swap(int *x,int *y)
+//{
+//    int c;
+//    c=*x;
+//    *x=*y;
+//    *y=c;
+//    cout<<"The swapping is complete"<<endl;
+//}
+//int main(){
+//    int a,b;
+//    a=3;
+//    b=4;
+//    /*
+//    int c;
+//    c=*a;
+//    *a=*b;
+//    *b=c;
+//    */
+//    swap(&a,&b);
+//    cout<<"The value after the swap are"<<a<<b;
+//    return 0;
+//}
+
+
+
+//2.
+//int main(){
+//    int a[10];
+//    cout<<"values for comparison: "<<endl;
+//    cout<<"values of a is: "<<a<<endl;
+//    cout<<"values of address a[0] is: "<<&a[0]<<endl;
+//    cout<<"values of address a[1] is: "<<&a[1]<<endl;
+//
+//}
